7dbd05848391acff16851a9d4a42e5e89f09ceda
Biniam Gashaw <biniam@Biniams-Air.lan>
2024-01-09 01:49:15 -0800

    updated project

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@
 #define COLMFT2 20
 using namespace std;
 float conversion(float &, float &);
-string setLocation(int&);
-float rate(int&, int&);
+string setLocation(int &);
+float rate(int &, int &);
 float currency(int &);
 
+void standardToMetric();
+void metricToStandard();
 int main()
 {
     float currencyRate = 0;
@@ -22,27 +24,49 @@ int main()
     string firstLocation, secondLocation;
 
     cout << fixed << setprecision(2);
-    cout << endl << "Welcome to Currency Converter" << endl<< endl;
-    cout << "Please select starting Foreign Currency" << endl;
-    cout << "1. USD" << endl << "2. Euro" << endl << "3. Yen" << endl <<  "4. Pound" << endl <<"5. Franc" <<endl << "0. Exit" << endl << "Choice: ";
-    cin >> first;
-    firstLocation = setLocation(first);
-    startingCurrency = currency(first);
-    cout << "Please select conversion Foreign Currency" << endl;
-    cout << "1. USD" << endl << "2. Euro" << endl << "3. Yen" << endl <<  "4. Pound" << endl <<"5. Franc" <<endl << "0. Exit" << endl << "Choice: ";
-    cin >> second;
-    secondLocation = setLocation(second);
-    currencyRate = rate(first, second);
+    cout << endl << "Welcome to Currency/Metric Units Converter" << endl << endl;
+    cout << "Would you like to convert 1. units or 2. currency? :";
+    int choice;
+    cin >> choice;
+    if (choice == 1)
+    {
+        cout << "Would you like to convert from 1. (Standard to Metric) or 2. (Metric to Standard): ";
+        int secondChoice;
+        cin >> secondChoice;
 
-    cout << setw(COLMFT1) << left << firstLocation;
-    cout << setw(COLMFT2) << right << fixed << setprecision(2) << startingCurrency << endl;
-    cout << "Conversion rate(per "<< secondLocation << "):    ";
-    cout << setw(COLMFT2) << right << currencyRate << "%" << endl;
-    total = conversion(startingCurrency, currencyRate);
-    cout << secondLocation << "    ";
-    cout << setw(COLMFT1) << right << total << endl;
-    cout << "\n--------------------------- " << endl;
-    cout << "\nEnd of Rate Converter" << endl;
+        if (secondChoice == 1)
+        {
+            standardToMetric();
+        }
+        else if (secondChoice == 2)
+        {
+            metricToStandard();
+        }
+    }
+    else
+    {
+
+        cout << "Please select starting Foreign Currency" << endl;
+        cout << "1. USD" << endl << "2. Euro" << endl << "3. Yen" << endl << "4. Pound" << endl << "5. Franc" << endl << "0. Exit" << endl << "Choice: ";
+        cin >> first;
+        firstLocation = setLocation(first);
+        startingCurrency = currency(first);
+        cout << "Please select conversion Foreign Currency" << endl;
+        cout << "1. USD" << endl << "2. Euro" << endl << "3. Yen" << endl << "4. Pound" << endl << "5. Franc" << endl << "0. Exit" << endl << "Choice: ";
+        cin >> second;
+        secondLocation = setLocation(second);
+        currencyRate = rate(first, second);
+
+        cout << setw(COLMFT1) << left << firstLocation;
+        cout << setw(COLMFT2) << right << fixed << setprecision(2) << startingCurrency << endl;
+        cout << "Conversion rate(per " << secondLocation << "):    ";
+        cout << setw(COLMFT2) << right << currencyRate << "%" << endl;
+        total = conversion(startingCurrency, currencyRate);
+        cout << secondLocation << "    ";
+        cout << setw(COLMFT1) << right << total << endl;
+        cout << "\n--------------------------- " << endl;
+        cout << "\nEnd of Rate Converter" << endl;
+    }
 }
 
 float conversion(float &startingCurrency, float &rate)
@@ -95,110 +119,211 @@ float currency(int &first)
     return inputValue;
 }
 
-
-float rate(int& first, int& second){
+float rate(int &first, int &second)
+{
     float rate = 0;
-    if(first == 1 && second == 1){
+    if (first == 1 && second == 1)
+    {
         rate = 1.00;
     }
-    else if(first == 1 && second == 2){
+    else if (first == 1 && second == 2)
+    {
         rate = 0.91;
     }
-    else if(first == 1 && second == 3){
+    else if (first == 1 && second == 3)
+    {
         rate = 144.15;
     }
-    else if(first == 1 && second == 4){
+    else if (first == 1 && second == 4)
+    {
         rate = 0.78;
     }
-    else if(first == 1 && second == 5){
+    else if (first == 1 && second == 5)
+    {
         rate = 0.85;
     }
-    else if(first == 2 && second == 1){
+    else if (first == 2 && second == 1)
+    {
         rate = 1.10;
     }
-    else if(first == 2 && second == 2){
+    else if (first == 2 && second == 2)
+    {
         rate = 1.00;
     }
-    else if(first == 2 && second == 3){
+    else if (first == 2 && second == 3)
+    {
         rate = 157.90;
     }
-    else if(first == 2 && second == 4){
+    else if (first == 2 && second == 4)
+    {
         rate = 0.86;
     }
-    else if(first == 2 && second == 5){
+    else if (first == 2 && second == 5)
+    {
         rate = 0.93;
     }
-    else if(first == 3 && second == 1){
+    else if (first == 3 && second == 1)
+    {
         rate = 0.0069;
     }
-    else if(first == 3 && second == 2){
+    else if (first == 3 && second == 2)
+    {
         rate = 0.0063;
     }
-    else if(first == 3 && second == 3){
+    else if (first == 3 && second == 3)
+    {
         rate = 1.00;
     }
-    else if(first == 3 && second == 4){
+    else if (first == 3 && second == 4)
+    {
         rate = 0.0054;
     }
-    else if(first == 3 && second == 5){
+    else if (first == 3 && second == 5)
+    {
         rate = 0.0059;
     }
-    else if(first == 4 && second == 1){
+    else if (first == 4 && second == 1)
+    {
         rate = 1.28;
     }
-    else if(first == 4 && second == 2){
+    else if (first == 4 && second == 2)
+    {
         rate = 1.16;
     }
-    else if(first == 4 && second == 3){
+    else if (first == 4 && second == 3)
+    {
         rate = 183.87;
     }
-    else if(first == 4 && second == 4){
+    else if (first == 4 && second == 4)
+    {
         rate = 1.00;
     }
-    else if(first == 4 && second == 5){
+    else if (first == 4 && second == 5)
+    {
         rate = 1.09;
     }
-    else if(first == 5 && second == 1){
+    else if (first == 5 && second == 1)
+    {
         rate = 1.18;
     }
-    else if(first == 5 && second == 2){
+    else if (first == 5 && second == 2)
+    {
         rate = 1.08;
     }
-    else if(first == 5 && second == 3){
+    else if (first == 5 && second == 3)
+    {
         rate = 170.07;
     }
-    else if(first == 5 && second == 4){
+    else if (first == 5 && second == 4)
+    {
         rate = 0.92;
     }
-    else if(first == 5 && second == 5){
+    else if (first == 5 && second == 5)
+    {
         rate = 1;
     }
-    else{
+    else
+    {
         rate = -1.11;
     }
-return rate;
+    return rate;
 }
 
-
-string setLocation(int& choice){
-    if(choice == 1){
+string setLocation(int &choice)
+{
+    if (choice == 1)
+    {
         return "USD($)";
     }
-    else if(choice == 2){
+    else if (choice == 2)
+    {
         return "Euros(€)";
     }
-    else if(choice == 3){
+    else if (choice == 3)
+    {
         return "Yen(¥)";
     }
-    else if(choice == 4){
+    else if (choice == 4)
+    {
         return "Pounds(£)";
     }
-    else if(choice == 5){
+    else if (choice == 5)
+    {
         return "Franc(ƒ)";
     }
-    else{
+    else
+    {
         return "invalid location";
     }
+}
 
+void standardToMetric()
+{
+    float standardUnit = 0.00;
+    float conversionRate = 0.00;
+    int metricChoice;
+    cout << "Input standard unit: ";
+    cin >> standardUnit;
+    cout << endl << "1. Miles to Kilometers" << endl << "2. Feet to Meters" << endl << "3. Miles to Meters" << endl << "4. Feet to Kilometers" << endl << "5. Fahrenheit to Celsius: ";
+    cin >> metricChoice;
+    switch (metricChoice)
+    {
 
+    case 1:
+        conversionRate = 1.60934;
+        standardUnit *= conversionRate;
+        break;
+    case 2:
+        conversionRate = 0.3048;
+        standardUnit *= conversionRate;
+        break;
+    case 3:
+        conversionRate = 1609.34;
+        standardUnit *= conversionRate;
+        break;
+    case 4:
+        conversionRate = 3280.84;
+        standardUnit /= conversionRate;
+        break;
+
+    case 5:
+        
+        standardUnit = (standardUnit - 32) / 1.8;
+        break;
+    }
+    cout << endl << "Standard to Metric: " << standardUnit << endl;
+}
+void metricToStandard()
+{
+    float standardUnit = 0.00;
+    float conversionRate = 0.00;
+    int standardChoice;
+    cout << "Input Metric unit: ";
+    cin >> standardUnit;
+    cout << endl << "1. Kilometers to Miles" << endl << "2. Meters to Feet" << endl << "3. Meters to Miles" << endl << "4. Kilometers to Feet" << endl << "5. Celsius to Farenheit: ";
+    cin >> standardChoice;
+    switch (standardChoice)
+    {
+
+    case 1:
+        conversionRate = 0.621371;
+        standardUnit *= conversionRate;
+        break;
+    case 2:
+        conversionRate = 3.28084;
+        standardUnit *= conversionRate;
+        break;
+    case 3:
+        conversionRate = 0.000621371;
+        standardUnit *= conversionRate;
+        break;
+    case 4:
+        conversionRate = 3280.84;
+        standardUnit *= conversionRate;
+        break;
+    case 5:
+        standardUnit = (standardUnit * 1.8) + 32;
+        break;
+    }
+    cout << endl << "Metric to Standard: " << standardUnit << endl;
 }
